cd2744ab9ee3e5b3a4777744b7e125873ca8d47c
GutoL <guto-acmilan@hotmail.com>
2015-09-12 22:44:06 -0300

    OK

--- servicos/historicoEscolar/principal.php
+++ servicos/historicoEscolar/principal.php
@@ -19,31 +19,6 @@
 		
 		<?php
 			
-			/*//Pegando informações do usuário
-			require_once('../../protected/vendors/yii/yii.php');
-			Yii::createWebApplication('../../protected/config/main.php');
-			if(!Yii::app()->user->isGuest){
-			$user_id = Yii::app()->user->id;
-			
-			}
-			include "conexaoBanco.php";
-			
-			$sql = "SELECT * FROM profile WHERE user_id = ".$user_id;
-			$resultado = mysqli_query($conexao,$sql); // conexao vem do include
-			
-			$situacao_historico;
-			$nome_usuario;
-			$cpf_usuario;
-			
-			while($row = mysqli_fetch_array($resultado)){
-				
-					$situacao_historico = $row["emissao_historico"];
-					$nome_usuario = $row["firstname"]." ".$row["lastname"];
-					$cpf_usuario = $row["cpf"];
-			
-			}
-			
-			mysqli_free_result($resultado);*/
 
 			include "conexaoBanco.php";
 			
--- servicos/historicoEscolar/salvarPedido2.php
+++ servicos/historicoEscolar/salvarPedido2.php
@@ -31,7 +31,7 @@
 				
 				mysqli_free_result($resultado);
 				
-				echo "<form action=\"form.php\" method=\"post\">
+				echo "<form action=\"principal.php\" method=\"post\">
 							<p>Este código já foi utilizado!</p>
 							
             				<input type=\"submit\" value=\"Voltar\" class=\"button special\">
@@ -43,8 +43,21 @@
 				
 				mysqli_free_result($resultado);
 				
-				$sql = "INSERT INTO `codigos_historicos_escolares` (`codigo`)VALUES";
-				$sql = $sql. "('$codigo')";
+				$sql = "SELECT id FROM `historico_escolar` ORDER BY id DESC LIMIT 1"; 
+				$resultado = mysqli_query($conexao,$sql);
+				$id_max;
+				
+				While($row = mysqli_fetch_array($resultado)){
+					
+					$id_max = $row["id"];
+					
+				}
+				
+				$id_max = $id_max + 1;
+				
+				
+				$sql = "INSERT INTO `codigos_historicos_escolares` (`codigo`,`id_historico`)VALUES";
+				$sql = $sql. "('$codigo','$id_max')";
 				$resultado = mysqli_query($conexao,$sql);
 				
 				include "salvarPedido1.php";
