10f2d97b099ad57865b91e72f8fc09c4b34303f2
GutoL <guto-acmilan@hotmail.com>
2015-10-13 19:44:16 -0300

    OK

--- servicos/historicoEscolar/conexaoBanco.php
+++ servicos/historicoEscolar/conexaoBanco.php
@@ -1,11 +1,18 @@
 <?php 
 
 	// Informações do Banco de Dados
-	$hostname = "localhost";
+	/*$hostname = "localhost";
 	$usuario = "root";
 	$senha = "";
-	$banco = "followedu2";
-	$conexao = mysqli_connect($hostname, $usuario, $senha, $banco) or die ("Erro ao conectar!");
+	$banco = "followedu2";*/
+	
+	$servername = "renanfelixrodrigues.com.br";
+	$username = "renan549_upe";
+	$password = "upe2015";
+	$dbname = "renan549_followedu";
+	include("../../database/datanoob.php");
+	
+	$conexao = mysqli_connect($servername, $username, $password, $dbname) or die ("Erro ao conectar!");
 	
        
 	// Check connection
